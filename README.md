db11ebef73375e8244d561d7a8a469b2a4b22713
Victor "multun" Collod <victor.collod@epita.fr>
2017-10-09 23:23:14 +0200

    pip: add README and setup.py

---
+++ README.md
@@ -0,0 +1,19 @@
+# Introduction
+
+Trish is an attempt to detect similarities between two text files.
+The algorithm is fairly simple and will be described sooned or later.
+
+It basicaly works by replacing each localy unique sequence of letters
+by a number.
+
+# Usage
+
+`Usage: trish fileA fileB`
+
+# Setup
+
+Depends on python 3.6+
+
+```
+pip3 install git+git://github.com/multun/trish.git
+```
---
+++ setup.py
@@ -0,0 +1,25 @@
+import os
+from setuptools import setup, find_packages
+
+def read(shortname):
+    filename = os.path.join(os.path.dirname(__file__), shortname)
+    with open(filename, encoding='utf-8') as f:
+        contents = f.read()
+    return contents
+
+setup(
+    name="trish",
+    version="0.0.1",
+    author="multun",
+    scripts=['trish'],
+    description="A cheating detection tool.",
+    keywords="cheating",
+    url="https://github.com/multun/trish",
+    packages=find_packages(),
+    python_requires='>= 3.6',
+    install_requires=[],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+    ],
+)
